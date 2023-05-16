da0751a9cb0d660ba20e43e534e74c68f08108e5
Derek Wheeler <frozenarrow73@gmail.com>
2023-05-16 15:11:12 -0600

    setup frontend login

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,18 +1,54 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import AuthContext from '../store/authContext'
+import axios from 'axios'
 
 function Login() {
     const authContext = useContext(AuthContext)
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value)
+    }
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        const body = {
+            username,
+            password
+        }
+
+        setPassword("")
+        setUsername("")
+
+        if(authContext.register) {
+            axios.post("http://localhost:4000/api/register", body).then((req, res) => {
+                console.log(req.data)
+            }).catch((err) => {
+                console.log(err)
+            })
+        } else {
+            axios.post("http://localhost:4000/api/login", body).then((req, res) => {
+                console.log(req.data)
+            }).catch((err) => {
+                console.log(err)
+            }) 
+        }
+    }
   return (
-    <form className='loginForm'>
+    <form className='loginForm' onSubmit={handleSubmit}>
         <h2>{authContext.register?"Register":"Login"}</h2>
         <div className='loginDiv'>
-            <label htmlFor="username">Username</label>
-            <input type="text" id='username' />
+            <label htmlFor="username" >Username</label>
+            <input type="text" id='username' value={username} onChange={handleUsernameChange}/>
             <label htmlFor="password">Password</label>
-            <input type="password" id='password' />
+            <input type="password" id='password' value={password} onChange={handlePasswordChange}/>
         </div>
-        <button type='submit'>Login</button>
+        <button type='submit'>Submit</button>
     </form>
   )
 }
