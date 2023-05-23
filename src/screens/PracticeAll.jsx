f0d8f8464f2f46a4bb52a1a79ad62b81ff806c03
Derek Wheeler <frozenarrow73@gmail.com>
2023-05-23 10:45:31 -0600

    setup front end getRandomSentence axios call

--- src/screens/PracticeAll.jsx
+++ src/screens/PracticeAll.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, {useEffect, useContext} from 'react'
+import axios from 'axios'
+import AuthContext from '../store/authContext'
 
 function PracticeAll() {
+  const authContext = useContext(AuthContext)
+  useEffect(()=>{
+    axios.get(`/api/getRandomSentence/${authContext.userId}`, {headers: {authorization: authContext.token}}).then((res)=>{
+      console.log(res.data)
+    })
+  }, [])
   return (
     <div>PracticeAll</div>
   )
